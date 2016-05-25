d108a22fb181d160cef21028a3520f3d37bdc136
akhilchandran.t.r <akhilchandran.t.r@gmail.com>
2016-05-25 13:26:35 +0530

    insert replacing the leaf node which is null

--- redblacktree.c
+++ redblacktree.c
@@ -15,3 +15,30 @@ int is_red(struct node *head)
 {
 	return (head == NULL)? 0: head->color;
 }
+
+int insertnode(struct node **treeheadRef,int data)
+{
+	
+	if(*treeheadRef == NULL){
+		struct node *newnode = malloc(sizeof(struct node));
+		newnode->data = data;
+		newnode->color = RED;
+		newnode->left = NULL;
+		newnode->right = NULL;
+		*treeheadRef = newnode;
+	}
+
+}
+	
+	
+
+main()
+{
+	struct node *head = malloc(sizeof(struct node));
+	head->color = 1;
+	printf("%d\n",is_red(head));
+	head->color = 0;
+	printf("%d\n",is_red(head));
+	printf("%d\n",is_red(NULL));
+	
+}
